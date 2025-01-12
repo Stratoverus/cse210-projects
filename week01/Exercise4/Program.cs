359b0b8ee7c295f76c76921e29b073cd7fb6df5d
stratoverus <keitheberhard93@gmail.com>
2025-01-11 22:32:20 -0700

    Exercise 4 uploaded

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int number = 1;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Please enter a list of numbers. When you enter 0, this will finish.");
+        while (number != 0)
+        {
+            Console.Write("Please enter a number: ");
+            string response = Console.ReadLine();
+            number = int.Parse(response);
+
+            numbers.Add(number);
+
+        }
+        //Figure out the sum here
+        int total = numbers.Sum();
+        Console.WriteLine($"The total is {total}");
+        
+        //Figure out the average here
+        double average = numbers.Average();
+        Console.WriteLine($"The Average is {average}");
+
+        //Figure out the largest number
+        int largest = numbers.Max();
+        Console.WriteLine($"The Largest number is {largest}");
     }
 }
