c8f1d2c32ef75d1d7be16a3a987bc74554ed852a
stratoverus <keitheberhard93@gmail.com>
2025-01-11 19:26:51 -0700

    Finished first exercise

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //Get first name
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        //Get last name
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        //Put together
+        Console.WriteLine($"Your name is {last}, {first} {last}");
     }
 }
