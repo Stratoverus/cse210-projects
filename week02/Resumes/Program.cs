c6e3002216fdb0738ac1e5ae951b8ba68d1237b3
stratoverus <keitheberhard93@gmail.com>
2025-01-16 22:37:33 -0700

    testing program and the newly added resume section

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -8,16 +8,22 @@ class Program
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
         job1._startYear = 2020;
-        job1._endYear = 2024;
+        job1._endYear = 2021;
         job1.Display();
 
         Job job2 = new Job();
         job2._jobTitle = "QA Engineer";
         job2._company = "Early Warning";
         job2._startYear = 2021;
-        job2._endYear = 2022;
+        job2._endYear = 2023;
         job2.Display();
 
+        Resume myResume = new Resume();
+        myResume._name = "Keith Eberhard";
 
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        Console.WriteLine(myResume._jobs[1]._jobTitle);
     }
 }
