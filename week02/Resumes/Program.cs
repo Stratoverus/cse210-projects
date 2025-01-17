351880f43783c8e4ad0c5ba37e461b53ff3e1186
stratoverus <keitheberhard93@gmail.com>
2025-01-16 22:24:57 -0700

    added second job in program

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -6,6 +6,18 @@ class Program
     {
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
-        Console.WriteLine(job1._jobTitle);
+        job1._company = "Microsoft";
+        job1._startYear = 2020;
+        job1._endYear = 2024;
+        Console.WriteLine(job1._company);
+
+        Job job2 = new Job();
+        job2._jobTitle = "QA Engineer";
+        job2._company = "Microsoft";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+        Console.WriteLine(job2._company);
+
+
     }
 }
