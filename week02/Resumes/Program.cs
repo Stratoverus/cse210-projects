e70be5d11b90a018e81b701fc20f42c3be71a469
stratoverus <keitheberhard93@gmail.com>
2025-01-16 22:29:50 -0700

    created Display Method and called the method in program.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -9,14 +9,14 @@ class Program
         job1._company = "Microsoft";
         job1._startYear = 2020;
         job1._endYear = 2024;
-        Console.WriteLine(job1._company);
+        job1.Display();
 
         Job job2 = new Job();
         job2._jobTitle = "QA Engineer";
         job2._company = "Early Warning";
         job2._startYear = 2021;
         job2._endYear = 2022;
-        Console.WriteLine(job2._company);
+        job2.Display();
 
 
     }
