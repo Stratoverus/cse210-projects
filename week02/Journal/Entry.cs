6ad6a563d60e96f06c02fa2ec90754c51b293422
stratoverus <keitheberhard93@gmail.com>
2025-01-18 17:20:16 -0700

    To make it easier to import files in the future, I made the output of the entry one line.

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -9,7 +9,6 @@ public class Entry
 
     public void Display()
     {
-        Console.WriteLine($"{_date}");
-        Console.WriteLine($"{_promptText}: {_entryText}");
+        Console.WriteLine($"{_date} - {_promptText}: {_entryText}");
     }
 }
