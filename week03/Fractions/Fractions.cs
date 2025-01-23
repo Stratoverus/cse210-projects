eeb70a613fdb9a4cad30b70c1dadacf7489ee990
stratoverus <keitheberhard93@gmail.com>
2025-01-22 21:10:54 -0700

    added constructors portions

--- week03/Fractions/Fractions.cs
+++ week03/Fractions/Fractions.cs
@@ -2,5 +2,24 @@ class Fractions
 {
     private int _top;
     private int _bottom;
+
+    public Fractions()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fractions(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fractions(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
     
 }
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fractions fractions = new();
+        Fractions fractions1 = new(5);
+        Fractions fractions2 = new(3, 4);
     }
 }
