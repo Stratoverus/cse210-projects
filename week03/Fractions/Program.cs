e19616e37e464a1bd73e15869c1c7c003a4b85c1
stratoverus <keitheberhard93@gmail.com>
2025-01-22 21:17:23 -0700

    Added calls to display the fractions.

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,7 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Fractions fractions = new();
+        Console.WriteLine(fractions.GetFractionString());
+        Console.WriteLine(fractions.GetDecimalValue());
+
         Fractions fractions1 = new(5);
+        Console.WriteLine(fractions1.GetFractionString());
+        Console.WriteLine(fractions1.GetDecimalValue());
+
         Fractions fractions2 = new(3, 4);
+        Console.WriteLine(fractions2.GetFractionString());
+        Console.WriteLine(fractions2.GetDecimalValue());
+
     }
 }
