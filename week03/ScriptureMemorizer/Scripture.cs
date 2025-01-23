13d397ff379971785c79deed8a11e570ca043cb8
stratoverus <keitheberhard93@gmail.com>
2025-01-22 21:36:30 -0700

    Finished up Scripture and Reference blocking.

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,4 +1,25 @@
 class Scripture
 {
-    
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return true;
+    }
 }
