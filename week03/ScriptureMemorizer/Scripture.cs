cfd8a159dad0c46450f90714bddd9dc984c83e65
stratoverus <keitheberhard93@gmail.com>
2025-01-25 16:17:36 -0700

    Moving the scripture text to Program as I didn't understand what needed to happen.

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,14 +1,17 @@
 class Scripture
 {
     private Reference _reference;
-    private List<Word> _words = new() {"And when ye shall receive these things, I would exhort you that ye would ask God, the Eternal Father, in the name of Christ, fi these things are not true; and if ye shall ask with a sincere heart, with real intent, having faith in Christ, he will manifest the truth of it unto you, by the power of the Holy ghost. And by the power of the Holy Ghost ye may know the truth of all things."};
+    // Scripture: And when ye shall receive these things, I would exhort you that ye would ask God, the Eternal Father, in the name of Christ, fi these things are not true; and if ye shall ask with a sincere heart, with real intent, having faith in Christ, he will manifest the truth of it unto you, by the power of the Holy ghost. And by the power of the Holy Ghost ye may know the truth of all things.
+    private List<Word> _words = new();
+    
 
     public Scripture(Reference reference, string text)
     {
+        string[] wordsArray = text.Split(' ');
         _reference = reference;
-        foreach (Word i in _words)
+        foreach (string i in wordsArray)
         {
-            
+            _words.Add(new Word(i));
         }
     }
 
