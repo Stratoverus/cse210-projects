772147c190f31d229c0abd1b74f4da9313eddcab
stratoverus <keitheberhard93@gmail.com>
2025-01-24 19:36:34 -0700

    Starting to program the assignment.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,9 +1,11 @@
+//Scripture memorize Program
+//I went above and beyond by having there be multiple different scriptures that it'll randomly select from.
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        
     }
 }
