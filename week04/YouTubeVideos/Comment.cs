9031d7b68664b331adabec71ad4425a5ebae9f86
stratoverus <keitheberhard93@gmail.com>
2025-01-29 21:26:43 -0700

    fleshed out comment class

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -2,8 +2,14 @@ class Comment
 {
     public string _name;
     public string _text;
+
+    public Comment(string name, string text)
+    {
+        _name = name;
+        _text = text;
+    }
     public void Display()
     {
-        
+        Console.WriteLine($"{_name}: {_text}");
     }
 }
