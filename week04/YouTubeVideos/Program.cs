61548257e5ad57da4822067526336c28996fab18
stratoverus <keitheberhard93@gmail.com>
2025-01-29 22:39:26 -0700

    Working to add video details.

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -5,7 +5,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Video myVideo = new();
-        
+        Video video1 = new();
+        video1._title = "The Hunt for the BEST Controller";
+        video1._author = "Linus Tech Tips";
+        video1._length = 1488;
+        video1._comments.Add("BocchiTheBox", "This is probably the best advertisement these controller companies never asked for.");
+
+        Video video2 = new();
+
+
+        Video video3 = new();
+
+
     }
 }
