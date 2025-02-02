63416f55e0d3aa0b7d4ed047f3c9b6ca6e5e0e24
stratoverus <keitheberhard93@gmail.com>
2025-02-01 17:35:53 -0700

    It's making progress, not there yet. We're getting there.

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,12 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Order myOrder1 = new();
-        myOrder1._customer = new Customer("Keith Eberhard", new Address("1841 W. Expressman St.", "Apache Junction", "Arizona", "USA"));
-        myOrder1._products.Add(new Product("Laptop", 121, 1299.99, 1));
-        
+        Address myAddress1 = new("1841 W. Expressman St.", "Apache Junction", "Arizona", "USA");
+        Customer myCustomer1 = new("Keith Eberhard", myAddress1);
+        Product myProduct1 = new("Laptop", 121, 1299.99, 1);
+        Product myProduct2 = new("Desktop Computer", 120, 1999.99, 2);
+        List<Product> products1 = new(myProduct1, myProduct2);
+        Order myOrder1 = new(myCustomer1, products1);
 
-        Order myOrder2 = new();
+        Address myAddress2 = new("2898 W. 820 N.", "Provo", "Utah", "UK");
+        Customer myCustomer2 = new("British Keith", myAddress2);
+        Product myProduct2 = 
               
     }
 }
