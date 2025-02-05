9e5107b81285620006c35a85036e215464c2374e
stratoverus <keitheberhard93@gmail.com>
2025-02-04 23:20:56 -0700

    Added the object in program.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,7 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Assignment myAssignment = new("Keith Eberhard", "Programming");
+        Assignment myAssignment = new("Keith Eberhard", "Calculus");
         Console.WriteLine(myAssignment.GetSummary());
+        MathAssignment myMath = new("Keith Eberhard","Calculus","54", "1-20");
+        Console.WriteLine(myMath.GetHomeworkList());
     }
 }
