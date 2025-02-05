b8f06fa0e980c26dfc81b94f34b1dd83498d3cbe
stratoverus <keitheberhard93@gmail.com>
2025-02-04 23:17:18 -0700

    Filled out more of the assignment class.

--- week05/Homework/Assignment.cs
+++ week05/Homework/Assignment.cs
@@ -1,4 +1,4 @@
-class Assignment
+public class Assignment
 {
     private string _studentName;
     private string _topic;
@@ -11,5 +11,21 @@ class Assignment
     public string GetSummary()
     {
         return $"{_studentName} - {_topic}";
+    }
+
+    
+}
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems) : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
     }
 }
