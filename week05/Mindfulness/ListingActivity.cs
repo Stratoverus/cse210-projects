5bba0d1126951255af871c629637cae077bc7daa
stratoverus <keitheberhard93@gmail.com>
2025-02-05 09:07:53 -0700

    more stubbed out

---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,24 @@
+class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts;
+    public ListingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public void GetRandomPrompt()
+    {
+
+    }
+
+    public List<string> GetListFromUser()
+    {
+        return [];
+    }
+}
---
+++ week05/Mindfulness/ReflectingActivity.cs
@@ -0,0 +1,36 @@
+using System.Net;
+
+class ReflectingActivity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+    public ReflectingActivity()
+    {
+
+    }
+
+    public void run()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+
+    public string GetRandomQuestion()
+    {
+        return "";
+    }
+
+    public void DisplayPrompt()
+    {
+
+    }
+
+    public void DisplayQuestions()
+    {
+        
+    }
+}
