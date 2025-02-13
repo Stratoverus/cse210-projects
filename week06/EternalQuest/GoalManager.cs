38781d2618c079ec68aef8394a8dac50fed76ef6
stratoverus <keitheberhard93@gmail.com>
2025-02-12 19:26:12 -0700

    Filled out the menu.

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -9,7 +9,49 @@ class GoalManager
 
     public void Start()
     {
+        while (true)
+        {
+            Console.Clear();
+            DisplayPlayerInfo();
+            Console.WriteLine();
+            Console.WriteLine("Please pick from one of the following options:");
+            Console.WriteLine("  1. Create New Goal");
+            Console.WriteLine("  2. List Goals");
+            Console.WriteLine("  3. Save Goals");
+            Console.WriteLine("  4. Load Goals");
+            Console.WriteLine("  5. Record Event");
+            Console.WriteLine("  6. Quit");
+            Console.WriteLine("What is your selection? ");
+            string answer = Console.ReadLine();
 
+            switch (answer)
+            {
+                case "1":
+                    CreateGoal();
+                    break;
+                case "2":
+                    ListGoalNames();
+                    break;
+                case "3":
+                    SaveGoals();
+                    break;
+                case "4":
+                    LoadGoals();
+                    break;
+                case "5":
+                    RecordEvent();
+                    break;
+                case "6":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("That is not a valid answer, try again.");
+                    Thread.Sleep(2000);
+                    Start();
+                    break;
+            }   
+
+        }
     }
 
     public void DisplayPlayerInfo()
@@ -44,6 +86,6 @@ class GoalManager
 
     public void LoadGoals()
     {
-        
+
     }
 }
--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,3 +1,5 @@
+//Here is the Eternal Quest program/game
+//I went above and beyond by doing....
 using System;
 
 class Program
@@ -5,5 +7,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager goal = new();
+        goal.Start();
     }
 }
