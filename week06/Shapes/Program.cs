d8667d4da2ea7ae81b3821368534dfc9c2410d1f
stratoverus <keitheberhard93@gmail.com>
2025-02-12 17:00:05 -0700

    more program filled out.

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square s1 = new(3, "Blue");
+
+        Console.WriteLine(s1);
     }
 }
