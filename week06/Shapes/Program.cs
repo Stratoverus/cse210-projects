658c8271d6d1c684fb4ae510e26ab06c7e2b61dc
stratoverus <keitheberhard93@gmail.com>
2025-02-12 18:26:31 -0700

    Added things to list

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,8 +5,18 @@ class Program
     static void Main(string[] args)
     {
         List<Shape> shapes = new();
+        
         Square s1 = new(3, "Blue");
+        shapes.Add(s1);
 
+        Rectangle s2 = new(4, 5, "red");
+        shapes.Add(s2);
+
+        Circle s3 = new(4, "purple");
+        shapes.Add(s3);
+            
         Console.WriteLine($"The color is {s1.Getcolor()} and the area is {s1.GetArea()}.");
+
+
     }
 }
