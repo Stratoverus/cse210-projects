88602141c93d33b75106c31bdf9bb7535f6a28f7
stratoverus <keitheberhard93@gmail.com>
2025-02-12 19:14:07 -0700

    missing stubs

--- week06/EternalQuest/ChecklistGoal.cs
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -8,4 +8,24 @@ class ChecklistGoal : Goal
         _target = target;
         _bonus = bonus;
     }
+
+    public override void RecordEvent()
+    {
+        base.RecordEvent();
+    }
+
+    public override bool IsComplete()
+    {
+        return base.IsComplete();
+    }
+
+    public override string GetDetailsString()
+    {
+        return base.GetDetailsString();
+    }
+    
+    public override string GetStringRepresentation()
+    {
+        return base.GetStringRepresentation();
+    }
 }
