de10d6509b619298c81c2cc2b16a059b7298af3f
stratoverus <keitheberhard93@gmail.com>
2025-02-12 18:17:10 -0700

    Making list

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<Shape> shapes = new();
         Square s1 = new(3, "Blue");
 
         Console.WriteLine($"The color is {s1.Getcolor()} and the area is {s1.GetArea()}.");
